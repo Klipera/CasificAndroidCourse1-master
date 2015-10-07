006bbc9f5c0ecf825c54d2e948daf70040428b22
Klipera <avivkliper@gmail.com>
2015-10-07 14:14:50 +0300

    Initial commit

---
+++ src/main/java/app/GlobalConstants.java
@@ -0,0 +1,16 @@
+package app;
+
+public class GlobalConstants {
+
+    public static final String STUDENTS_PHONE_NUMBER = "972526511777";
+    public static final String FACEBOOK_LINK = "https://www.facebook.com/aviv.kliper.77";
+    public static final String LINKEDIN_LINK = "https://www.linkedin.com";
+    public static final String YOUTUBE_LINK = "https://www.youtube.com";
+    public static final String CV_LINK = "https://www.gmail.com";
+    public static final String GMAIL_LINK = "https://www.gmail.com";
+    // Splash screen timer
+    public static int SPLASH_TIME_OUT = 1500;
+
+    // Web
+    public static final String CASIFIC_WEBSITE = "http://www.casific.com/";
+}
